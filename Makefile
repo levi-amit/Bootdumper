690ebdf15ec1a3eb623661e1b2e690108642b718
Amit Levi <amtlvi101@gmail.com>
2022-02-11 04:44:09 +0200

    Now possible to enter an array of segment addresses (in the assembly) to be dumped into a file in an orderly fashion!

--- Makefile
+++ Makefile
@@ -4,4 +4,5 @@ default:
 	nasm -fbin src/x86/bootloader.asm -o bin/bootdump.bin
 
 run:
+	qemu-img create output/floppy.img 16M
 	sudo qemu-system-x86_64 -hda bin/bootdump.bin -fda output/floppy.img -monitor stdio
--- src/x86/bootloader.asm
+++ src/x86/bootloader.asm
@@ -14,31 +14,44 @@ mov bp, 0x9C00
 mov sp, bp
 
 _start:
+    ; Segments are written to 0x12000 chunks of the floppy disk,
+    ; So if one segment starts at 0x0000 and ends at 0xffff,
+    ; the second will start at 0x12000 and end at 0x21fff, and so on.
     mov si, msg_welcome ; Print welcome message
     call puts
 
-    ; Print address of _start
-    mov si, msg_my_start_address_is
-    call puts
-    mov ax, cs
-    call print_ax
-    mov si, char_colon
-    call puts
-    mov ax, _start
-    call print_ax
+    xor cx, cx                      ; Start from first element of segments_to_dump
+    .dump_segments_loop:
+        ; Write message about current segment being dumped
+        push cx                         ; BP-2
+        mov si, msg_current_segment     ; "Current segment: 0x"
+        call puts
+        mov bx, [bp-2]
+        add bx, bx
+        mov ax, [segments_to_dump + bx]
+        call print_ax                   ; "0000"
+        mov si, newline
+        call puts
+
+        ; Write the current segment and progress to 
+        mov cx, [bp-2]                  ; Restore CX to be current loop index
+
+        mov bx, cx
+        add bx, bx
+        mov es, [segments_to_dump + bx] ; Select current segment to write to floppy
+        add cx, cx
+        mov ch, cl                      ; Start from floppy track CX * 2
+        mov cl, 1                       ; Start from floppy track first sector
+        call write_segment_floppyA
+
+        pop cx
+        inc cx                          ; increment index of current segment location in the segments_to_dump array
+        cmp cx, [segments_to_dump_len]  ; Check if current array index is the last
+        jb .dump_segments_loop          ; loop to dump each segment in the array
+
+
     mov si, newline
     call puts
-
-    ; Writing to floppy test
-    mov ax, ds
-    mov es, ax
-    mov bx, 0
-    call write_to_hd
-
-    call print_ax
-    mov si, newline
-    call puts
-
 
     ; Going through the IVT on the resulting dump of the first segment,
     ; We'll dump the segments which contain cool BIOS interrupt code
@@ -48,29 +61,68 @@ _start:
 
     jmp $                 ; wait
 
+; Array of segment addresses to be dumped.
+; Make sure to update the length data as you add elements!
+segments_to_dump: dw 0x0000
+segments_to_dump_len: dw 1
 
 ; Block Device Interaction
-write_to_hd:
-    ; Write data to Floppy disk 1
-    ; https://en.wikipedia.org/wiki/INT_13H
+write_segment_floppyA:
+    ; Write the contents of a full segment of memory to floppy A,
+    ; starting at given track and sector.
     ; ARGS:
-    ;   ES:BX: Buffer address pointer
-    push es
-    push bx
-    push dx
+    ;   ES: Segment to write to disk
+    ;   CH: Floppy track to start writing from
+    ;   CL: Sector in the track to start writing from
+    ; RETURN:
+    ;   AH: Last track written to
+    push bp             ; BP-0
+    mov bp, sp
+    push es             ; BP-2
+    push bx             ; BP-4
+    push dx             ; BP-6
+    
+    xor bx, bx          ; Start reading from beginning of segment
 
-    mov ah, 0x03        ; Select BIOS function - Write sectors to drive
-    mov al, 0x80        ; Sectors to write count - seems the maximum ix 0x80
-    mov ch, 0x00        ; Track
-    mov cl, 0x01        ; Sector
-    mov dh, 0x00        ; Head
-    mov dl, 0x00        ; Drive - 1st floppy disk
+    ; Calculate initial amount of sectors to write to fill up first track
+    mov al, 0x49        ; Max amount of sectors in a track + 1
+    sub al, cl          ; subtract first sector of first track (first sector of a track is 1 not 0)
 
-    int 0x13            ; BIOS interrupt - Block device action
+    dec sp
+    mov BYTE [bp-7], 0x80 ; BP-7: Stack_Var_1
+                        ; Counter for remaining sectors to write from the memory segment.
+                        ; Starts at 0xFFFF bytes / 512 bytes per sector = 0x7F sectors.
+    .write_loop:
+        ; Write current ES:BX to starting segment, until the end of the track
+        mov dl, 0x00    ; Drive - 1st floppy disk
+        mov dh, 0x00    ; Head - 0
+        mov ah, 0x03    ; Select BIOS function - Write sectors to drive
+        int 0x13        ; BIOS Interrupt - write AL sectors starting from memory ES:BX
+        ; AL now contains the amount of sectors written.
+        ; Set Stack_Var_1 to be new counter for remaining sectors
+        sub BYTE [bp-7], al                 ; Stack_Var_1 -= AL
+        ; Set BX to be next starting address in memory to read from
+        ; when writing to the next track
+        xor ah, ah                          ; AX = (int) AL
+        mul WORD [floppy_sector_size]       ; DX:AX = AX * 0x200 (Size of one sector)
+        add bx, ax                          ; BX += AL * 0x200 (amount of bytes written)
+        ; Set CL to write from next track start
+        mov cl, 1                           ; CL = 1
+        ; Set AL to the correct amount of sectors to write until the end of the segment
+        mov al, [bp-7]                      ; AL = Stack_Var_1
+        ; Increment track number
+        inc ch
+        ; Test if there are more sectors to write and stop loop if not
+        or al, al                           ; ZF = (AL==0)
+        jnz .write_loop
+    
+    mov ah, ch          ; Return last track written to
 
-    pop dx
-    pop bx
-    pop es
+    inc sp              ; pop Stack_Var_1
+    pop dx              ; BP-6
+    pop bx              ; BP-4
+    pop es              ; BP-2
+    pop bp              ; BP-0
     ret
 
 
@@ -191,7 +243,8 @@ newline: db 0x0D, 0x0A, 0
 msg_my_start_address_is: db "My _start address is: ", 0
 msg_current_segment: db "Dumping segment: 0x", 0
 char_colon: db ":", 0
+; Constants
+floppy_sector_size: dw 0x200
 
-
-times 510-($-$$) db 0x69
+times 510-($-$$) db 0
 dw 0xAA55               ; Magic bytes for the image to be considered a valid MBR
